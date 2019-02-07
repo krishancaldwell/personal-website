3bab9913eecb80538ea2ed9281221a474ef629c0
Krishan Caldwell <krishan@kcaldwell.me>
2019-02-07 22:12:00 +0900

    Converted Main back into a Component because I wanted to use the lifecycle functions. The reason for this was just to simulate where I would put an API call. Also added PropTypes in for better type protection

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,33 +1,60 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Title from './Title'
 import Menu from './Menu'
 import SocialSection from './SocialSection'
 
 
-const menuItems = [{
-    id: 0,
-    title: "Work",
-    link: ""
-  }, {
-    id: 1,
-    title: "Info",
-    link: ""
-  }, {
-    id: 2,
-    title: "Blog",
-    link: ""
-  }, {
-    id: 3,
-    title: "Contact",
-    link: ""
-  }]
+class Main extends Component {
 
-function Main() {
-  return <div className="container">
-           <Title title="Krishan Caldwell" />
-           <Menu items={menuItems}/>
-           <SocialSection />
-         </div>
+  constructor() {
+    super()
+    this.state = {
+      menuItems: []
+    }
+  }
+
+  componentDidMount() {
+    const data = simulateFetchFromDatabase()
+    this.setState({
+      menuItems: data
+    })
+  }
+
+  render() {
+    return <div className="container">
+             <Title title="Krishan Caldwell" />
+             <Menu items={this.state.menuItems}/>
+             <SocialSection />
+           </div>
+  }
+
+}
+
+/**
+ * This serves no purpose here. In reality if I were getting real stuff from a
+ * real DB then I'd put a function to get the data for that here...
+ * TODO: Actually make this one day
+ */
+function simulateFetchFromDatabase() {
+  return [
+    {
+      id: 0,
+      title: "Work",
+      link: ""
+    }, {
+      id: 1,
+      title: "Info",
+      link: ""
+    }, {
+      id: 2,
+      title: "Blog",
+      link: ""
+    }, {
+      id: 3,
+      title: "Contact",
+      link: ""
+    }
+  ]
 }
 
 export default Main
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import MenuItem from './MenuItem'
+import PropTypes from 'prop-types'
 
 function Menu(props) {
   return <div className="menu-section">
            {props.items.map((item, index) => <MenuItem key={index} item={item} />)}
          </div>
+}
+
+Menu.propTypes = {
+  items: PropTypes.array.isRequired
 }
 
 export default Menu
