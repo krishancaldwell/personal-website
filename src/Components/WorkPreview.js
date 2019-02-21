2fa4e9562fa1bf98eac6a9befc75f995cc37c5db
Krishan Caldwell <krishan@kcaldwell.me>
2019-02-21 17:51:26 +0900

    Updated the work page to include new app

--- src/Components/WorkPreview.js
+++ src/Components/WorkPreview.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import sosLogo from '../assets/sos_small.png'
 import qpayLogo from '../assets/qpay.png'
+import hnrLogo from '../assets/hacker_news_logo.png'
 
 
 function WorkPreview() {
@@ -15,6 +16,12 @@ function WorkPreview() {
            <a href="https://getqpay.com" target="_blank" rel="noopener noreferrer">
              <div className="work-entry">
                <img src={qpayLogo} alt="QPay" className="company-logo"/>
+             </div>
+           </a>
+
+           <a href="https://github.com/kthecaldwell/android-hacker-news-reader" target="_blank">
+             <div className="work-entry">
+               <img src={hnrLogo} alt="Hacker News Android app" className="company-logo"/>
              </div>
            </a>
          </div>
